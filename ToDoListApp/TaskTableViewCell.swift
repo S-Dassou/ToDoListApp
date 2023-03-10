3edae7010697d8ffcee65db330f7fc97a2aaead4
Shafique Dassu <shafd1231@gmail.com>
2023-03-10 09:53:09 +0100

    Added delegate to change task completion state.

--- ToDoListApp/TaskTableViewCell.swift
+++ ToDoListApp/TaskTableViewCell.swift
@@ -12,8 +12,15 @@ class TaskTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var checkmarkButton: UIButton!
+    weak var delegate: ViewControllerDelegate?
+    var index = 0 
     
     @IBAction func checkmarkButtonTapped(_ sender: Any) {
+        print("cell was tapped")
+        guard let viewController = viewController else {
+            return
+        }
+        delegate?.toggleIsComplete(forindex: index)
     }
     
 }
--- ToDoListApp/ViewController.swift
+++ ToDoListApp/ViewController.swift
@@ -7,13 +7,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class Task {
+    let title: String
+    let category: String
+    var  isComplete: Bool
+    init(title: String, category: String, isComplete: Bool) {
+        self.title = title
+        self.category = category
+        self.isComplete = isComplete
     }
-
-
+    func toggleIsComplete() {
+        isComplete = !isComplete
+    }
 }
 
+protocol ViewControllerDelegate: AnyObject {
+    func toggleIsComplete(forindex index: Int)
+}
+
+class ViewController: UIViewController, ViewControllerDelegate {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    var tasks: [Task] = [
+    Task(title: "walk", category: "hobby", isComplete: false),
+    Task(title: "run", category: "leisure", isComplete: true)
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func toggleIsComplete(forindex index: Int) {
+        
+    }
+
+   
+}
+
+//MARK: - TableView data source
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let task = tasks[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath) as! TaskTableViewCell
+        
+        cell.titleLabel.text = task.title
+        cell.categoryLabel.text = task.category
+        cell.delegate = self // (initialise VC var)
+        cell.index = indexPath.row
+        
+        if task.isComplete {
+            cell.checkmarkButton.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
+        } else {
+            cell.checkmarkButton.setImage(UIImage(systemName: "circle"), for: .normal)
+        }
+        return cell
+    }
+}
+
+//MARK: - Change row height
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let taskSelected = tasks[indexPath.row]
+//        taskSelected.toggleIsComplete()
+//        tableView.reloadData() //could we put this into the toggle func?
+//    }
+ 
+}
+//if it was a class: 
+//let taskSelected = tasks[IndexPath.row]
+//taskSelected.isComplete = !taskSelected.isComplete
+//tableView.reloadData()
